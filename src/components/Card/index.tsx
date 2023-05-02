8418e71d00056e35ea96aabcbb00007c7ce5b16e
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-02 19:57:06 -0300

    Add support for clickable cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,10 +10,11 @@ interface Props {
   onClick?: () => void
 }
 
-export function Card ({ flipped = false, figure, style = CardStyle.ZIGZAG, onClick = () => null }: Props) {
+export function Card ({ flipped = false, figure, style = CardStyle.ZIGZAG, onClick = undefined }: Props) {
   return <div className={classNames({
     'card': true,
     'card-flipped': flipped,
+    'card-clickable': !!onClick,
     'card-zigzag': style == CardStyle.ZIGZAG
   })} onClick={onClick}>
     <div className='card-content'>
