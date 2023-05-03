2bb7e11fb5afeb5c25f17cba68d6f8cbe2aadfd7
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-03 11:14:12 -0300

    Add persist middleware to session store

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -1,5 +1,6 @@
 import { Card } from 'models';
 import { useStore } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import { createStore } from 'zustand/vanilla';
 
@@ -19,51 +20,57 @@ interface SessionStoreState  {
 }
 
 export const SessionStore = createStore(
-  immer<SessionStoreState>((set) => ({
-    activeCardNumber: -1,
-    cards: [],
-    counter: 0,
+  persist(
+    immer<SessionStoreState>((set) => ({
+      activeCardNumber: -1,
+      cards: [],
+      counter: 0,
 
-    clear() {
-      set((state: SessionStoreState) => {
-        state.activeCardNumber = -1;
-        state.cards = [];
-        state.counter = 0;
-      });
-    },
-
-    clearActiveCard() {
-      set((state: SessionStoreState) => {
-        state.activeCardNumber = -1;
-      });
-    },
-
-    setCards(cards) {
-      set((state: SessionStoreState) => {
-        state.cards = cards;
-      });
-    },
-
-    setActiveCard(cardNumber) {
-      set((state: SessionStoreState) => {
-        state.activeCardNumber = cardNumber;
-      });
-    },
-
-    setCardFlipped(cardNumber, flipped) {
-      set((state: SessionStoreState) => {
-        state.cards.forEach(sessionCard => {
-          if (sessionCard.card.number === cardNumber) {
-            sessionCard.flipped = flipped;
-          }
+      clear() {
+        set((state: SessionStoreState) => {
+          state.activeCardNumber = -1;
+          state.cards = [];
+          state.counter = 0;
         });
-      });
-    },
+      },
 
-    incrementCounter() {
-      set((state: SessionStoreState) => { state.counter++; });
-    },
-  }))
+      clearActiveCard() {
+        set((state: SessionStoreState) => {
+          state.activeCardNumber = -1;
+        });
+      },
+
+      setCards(cards) {
+        set((state: SessionStoreState) => {
+          state.cards = cards;
+        });
+      },
+
+      setActiveCard(cardNumber) {
+        set((state: SessionStoreState) => {
+          state.activeCardNumber = cardNumber;
+        });
+      },
+
+      setCardFlipped(cardNumber, flipped) {
+        set((state: SessionStoreState) => {
+          state.cards.forEach(sessionCard => {
+            if (sessionCard.card.number === cardNumber) {
+              sessionCard.flipped = flipped;
+            }
+          });
+        });
+      },
+
+      incrementCounter() {
+        set((state: SessionStoreState) => { state.counter++; });
+      },
+    })),
+    {
+      name: 'session-storage',
+      storage: createJSONStorage(() => sessionStorage)
+    }
+  )
 );
 
 export const useSessionStore = (selector: (state: SessionStoreState) => any) => useStore(SessionStore, selector);
