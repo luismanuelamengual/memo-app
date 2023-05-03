a0d80cbfb53c7e2812d0d46ccb8b2561e73b4718
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-03 10:50:19 -0300

    Add session store

---
+++ src/stores/session.ts
@@ -0,0 +1,67 @@
+import { Card } from 'models';
+import { useStore } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+import { createStore } from 'zustand/vanilla';
+
+type SessionCard = { card: Card, flipped: boolean };
+
+interface SessionStoreState  {
+  activeCardNumber: number;
+  cards: Array<SessionCard>;
+  counter: number;
+
+  clear: () => void;
+  clearActiveCard: () => void;
+  setCards: (cards: Array<SessionCard>) => void;
+  setActiveCard: (cardNumber: number) => void;
+  setCardFlipped: (cardNumber: number, flipped: boolean) => void;
+  incrementCounter: () => void;
+}
+
+export const SessionStore = createStore(immer<SessionStoreState>((set) => ({
+  activeCardNumber: -1,
+  cards: [],
+  counter: 0,
+
+  clear() {
+    set((state: SessionStoreState) => {
+      state.activeCardNumber = -1;
+      state.cards = [];
+      state.counter = 0;
+    });
+  },
+
+  clearActiveCard() {
+    set((state: SessionStoreState) => {
+      state.activeCardNumber = -1;
+    });
+  },
+
+  setCards(cards) {
+    set((state: SessionStoreState) => {
+      state.cards = cards;
+    });
+  },
+
+  setActiveCard(cardNumber) {
+    set((state: SessionStoreState) => {
+      state.activeCardNumber = cardNumber;
+    });
+  },
+
+  setCardFlipped(cardNumber, flipped) {
+    set((state: SessionStoreState) => {
+      state.cards.forEach(sessionCard => {
+        if (sessionCard.card.number === cardNumber) {
+          sessionCard.flipped = flipped;
+        }
+      });
+    });
+  },
+
+  incrementCounter() {
+    set((state: SessionStoreState) => { state.counter++; });
+  },
+})));
+
+export const useSessionStore = (selector: (state: SessionStoreState) => any) => useStore(SessionStore, selector);
