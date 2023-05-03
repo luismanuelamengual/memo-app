639091ed610869d1316ac18c92c4e96d05e7ef3a
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-03 19:10:56 -0300

    Fix Column component styles

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -5,11 +5,19 @@ import './index.scss';
 interface Props {
   children: ReactNode;
   className?: string;
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
 };
 
-export function Column({ children, className = '' }: Props) {
+export function Column({ children, xs = 0, sm = 0, md = 0, lg = 0, className = '' }: Props) {
   return <div className={classNames({
     'column': true,
+    [`column-xs-${xs}`]: xs > 0,
+    [`column-sm-${sm}`]: sm > 0,
+    [`column-md-${md}`]: md > 0,
+    [`column-lg-${lg}`]: lg > 0,
     [className]: !!className
   })}>{children}</div>;
 }
