65bd4d912ca5aa7ba1cfb1dc38192ccb524bf0bc
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-04 12:02:43 -0300

    Fix code in memo actions

--- src/actions/memo.ts
+++ src/actions/memo.ts
@@ -6,12 +6,11 @@ import { generateRandomElements, getEnumValues, shuffleArray } from 'utilities';
 export function startMemoGame() {
   const cardTheme = CardTheme.ZIGZAG;
   const numberOfCards = 12;
-  const numberOfFigurePairs = numberOfCards / 2;
-  const figures: Array<Figure> = generateRandomElements(getEnumValues(Figure) as Array<Figure>, numberOfFigurePairs);
+  const figures: Array<Figure> = generateRandomElements(getEnumValues(Figure) as Array<Figure>, numberOfCards / 2);
   const cardFigures = shuffleArray(figures.concat([...figures]));
-  const cards = cardFigures.map((figure, index) => ({ figure, theme: cardTheme, number: index + 1, flipped: false } as SessionCard));
+  const sessionCards = cardFigures.map((figure, index) => ({ figure, theme: cardTheme, number: index + 1, flipped: false } as SessionCard));
   const sessionStore = SessionStore.getState();
   sessionStore.clear();
-  sessionStore.setCards(cards);
+  sessionStore.setCards(sessionCards);
   goToGameRoom();
 }
