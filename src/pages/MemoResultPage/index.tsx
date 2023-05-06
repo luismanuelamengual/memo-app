901b03c9fc0b4070cd389abd4015f49f1e85e973
Luis Manuel Amengual <luismanuelamengual@gmail.com>
2023-05-06 17:30:07 -0300

    Memo Result route protection

--- src/pages/MemoResultPage/index.tsx
+++ src/pages/MemoResultPage/index.tsx
@@ -1,13 +1,15 @@
 import { Player } from '@lottiefiles/react-lottie-player';
 import { goToHomePage } from 'actions';
 import { Button, Page, Score, Text, TextType } from 'components';
+import { Path } from 'models';
+import { Navigate } from 'react-router-dom';
 import { useMemoStore } from 'stores';
 import './index.scss';
 
 export function MemoResultPage() {
-  const sessionScore = useMemoStore(state => state.session?.score);
-
-  return (
+  const { isSessionEnded, sessionScore } = useMemoStore(state => ({ isSessionEnded: state.isSessionEnded() ,sessionScore: state.session?.score}));
+  return (!isSessionEnded ?
+    <Navigate to={Path.HOME} replace /> :
     <Page id='memo-result-page'>
       <Text className='main-title' type={TextType.TITLE}>Memo App</Text>
       <div className='congratulations-container'>
